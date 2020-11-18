58b4e23c20d13683d696041b613ef520570ba476
cslg2016 <895941515@qq.com>
2020-11-18 13:01:28 +0800

    单tab组件封装

--- app/src/main/java/com/example/c_library/MainActivity.kt
+++ app/src/main/java/com/example/c_library/MainActivity.kt
@@ -5,11 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.c_library.demo.CLogDemoActivity
+import org.chen.chui.tab.bottom.ChTabBottom
+import org.chen.chui.tab.bottom.ChTabBottomInfo
 
+/**
+ *
+ * */
 class MainActivity : AppCompatActivity() , View.OnClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tabBottom = findViewById<ChTabBottom>(R.id.tab_bottom)
+        val homeInfo = ChTabBottomInfo(
+            "首页",
+            "fonts/iconfont.ttf",
+            getString(R.string.if_home),
+            null,
+            "#ff656667",
+            "#ffd44949"
+        )
+        tabBottom.setChTabInfo(homeInfo);
     }
 
     override fun onClick(v: View?) {
